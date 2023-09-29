adc75099e47528440aa62c05a58606a9c9eda5cb
Conor <duffy-c76@ulster.ac.uk>
2023-09-29 18:49:39 +0100

    Question 4

---
+++ src/Question4.java
@@ -0,0 +1,16 @@
+public class Question4 {
+    public static void main(String[] args) {
+
+        for(int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+
+
+
+        String[] fruits = {"Apple", "Banana", "Orange", "Grapes"};
+
+        for(String i : fruits){
+            System.out.println(i);
+        }
+    }
+}

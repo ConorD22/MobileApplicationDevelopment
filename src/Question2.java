05802cdce58fd1937f4ac273577f370ed028d6ac
Conor <duffy-c76@ulster.ac.uk>
2023-09-29 18:43:32 +0100

    Question 2

---
+++ src/Question2.java
@@ -0,0 +1,15 @@
+public class Question2 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        double quotient = a / b;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+    }
+}
